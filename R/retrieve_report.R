59abeaf17c19a167e283a8d3ece56c30420c60de
Cyranka <harrofacyranka@gmail.com>
2019-06-24 13:20:43 -0400

    Kill donations

--- R/retrieve_report.R
+++ R/retrieve_report.R
@@ -37,19 +37,19 @@ retrieve_report <- function(id_vector, name_vector, my_platform, excel_file){
   Sys.sleep(10)
   a_10 <- dma_ads_breakdown(id_vector, name_vector, my_platform)
   Sys.sleep(15)
-  a_11 <- donations(id_vector, name_vector,my_platform)
+#  a_11 <- donations(id_vector, name_vector,my_platform)
   Sys.sleep(10)
-  a_12 <- csr_interests(id_vector,name_vector,my_platform)
+  a_11 <- csr_interests(id_vector,name_vector,my_platform)
   
   
   
   report_list <- list(a_1,a_2,a_3,a_4,
                   a_5,a_6,a_7,a_8,
-                  a_9,a_10,a_11, a_12)
+                  a_9,a_10,a_11)
   
   names(report_list) <- c("income", "age", "state", "ethnicity",
                           "gender", "ideology","education", "sports",
-                          "music", "media_markets", "donations", "csr_interests")
+                          "music", "media_markets", "csr_interests")
   
   print("Writing report")
   writexl::write_xlsx(report_list, stringr::str_glue(excel_file, ".xlsx"))
